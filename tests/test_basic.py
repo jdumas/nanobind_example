334d2edff62a329be4495cbd589127d7439f9197
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2025-08-09 16:43:16 -0700

    Simplify example.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,27 +1,24 @@
 import nanobind_example as m
 
 def test_fail():
-    f = m.Foo()
-    f.x = 42
-    assert f.x == 42
     b = m.Bar()
     assert len(b.foos) == 0
 
     # Does not work
-    b.foos.append(f)
+    b.foos.append(42)
     assert len(b.foos) == 1
+    assert b.foos[0] == 42
 
 def test_pass():
-    f = m.Foo()
-    f.x = 42
-    assert f.x == 42
     b = m.Bar()
     assert len(b.foos) == 0
 
     # This works
     foos = b.foos
     assert len(foos) == 0
-    foos.append(f)
+    foos.append(42)
     assert len(foos) == 1
+    assert foos[0] == 42
     b.foos = foos
     assert len(b.foos) == 1
+    assert b.foos[0] == 42
