6bf4ac2cf1bfb66df190a9582fbb385304acdb8d
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2025-08-09 16:34:22 -0700

    Minimal example of mutable vector object.

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,10 +1,31 @@
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/vector.h>
+
+#include <string>
 
 namespace nb = nanobind;
 
 using namespace nb::literals;
 
+struct Foo {
+    int x;
+};
+
+struct Bar {
+    std::vector<Foo> foos;
+};
+
 NB_MODULE(nanobind_example_ext, m) {
     m.doc() = "This is a \"hello world\" example with nanobind";
+
+    nb::class_<Foo>(m, "Foo")
+        .def(nb::init<>())
+        .def_rw("x", &Foo::x)
+        .def("__repr__", [](const Foo &f) { return "Foo(" + std::to_string(f.x) + ")"; });
+
+    nb::class_<Bar>(m, "Bar")
+        .def(nb::init<>())
+        .def_rw("foos", &Bar::foos);
+
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
 }
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,27 @@
 import nanobind_example as m
 
-def test_add():
-    assert m.add(1, 2) == 3
+def test_fail():
+    f = m.Foo()
+    f.x = 42
+    assert f.x == 42
+    b = m.Bar()
+    assert len(b.foos) == 0
+
+    # Does not work
+    b.foos.append(f)
+    assert len(b.foos) == 1
+
+def test_pass():
+    f = m.Foo()
+    f.x = 42
+    assert f.x == 42
+    b = m.Bar()
+    assert len(b.foos) == 0
+
+    # This works
+    foos = b.foos
+    assert len(foos) == 0
+    foos.append(f)
+    assert len(foos) == 1
+    b.foos = foos
+    assert len(b.foos) == 1
