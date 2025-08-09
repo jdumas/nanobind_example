65c495f0b87ef235d5fa945e4df9eca24c40c588
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2025-08-09 16:59:09 -0700

    Update example.

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,4 +1,5 @@
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/bind_vector.h>
 #include <nanobind/stl/vector.h>
 
 #include <string>
@@ -7,14 +8,35 @@ namespace nb = nanobind;
 
 using namespace nb::literals;
 
-struct Bar {
-    std::vector<int> foos;
+struct Foo {
+    int x;
 };
+
+struct Bar {
+    std::vector<Foo> foos;
+};
+
+struct Baz {
+    std::vector<int> ints;
+};
+
+// Prefer binding over type caster for std::vector<Foo>
+NB_MAKE_OPAQUE(std::vector<Foo>);
 
 NB_MODULE(nanobind_example_ext, m) {
     m.doc() = "This is a \"hello world\" example with nanobind";
 
+    nb::bind_vector<std::vector<Foo>>(m, "FooVector");
+
+    nb::class_<Foo>(m, "Foo")
+        .def(nb::init<>())
+        .def_rw("x", &Foo::x);
+
     nb::class_<Bar>(m, "Bar")
         .def(nb::init<>())
         .def_rw("foos", &Bar::foos);
+
+    nb::class_<Baz>(m, "Baz")
+        .def(nb::init<>())
+        .def_rw("ints", &Baz::ints);
 }
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,24 +1,28 @@
 import nanobind_example as m
 
-def test_fail():
+
+def test_baz():
+    b = m.Baz()
+    assert len(b.ints) == 0
+
+    # Using std::vector<int> type caster, appending to b.ints fails to mutate b.ints
+    b.ints.append(42)
+    assert len(b.ints) == 1
+
+
+def test_bar():
     b = m.Bar()
     assert len(b.foos) == 0
 
-    # Does not work
-    b.foos.append(42)
-    assert len(b.foos) == 1
-    assert b.foos[0] == 42
+    f = m.Foo()
+    f.x = 42
+    assert f.x == 42
 
-def test_pass():
-    b = m.Bar()
-    assert len(b.foos) == 0
-
-    # This works
-    foos = b.foos
-    assert len(foos) == 0
-    foos.append(42)
-    assert len(foos) == 1
-    assert foos[0] == 42
-    b.foos = foos
+    # Using std::vector<Foo> bindings, appendings to b.foos correctly mutates b.foos
+    b.foos.append(f)
     assert len(b.foos) == 1
-    assert b.foos[0] == 42
+    assert b.foos[0].x == 42
+
+    # However, mutating an element returned by a std::vector<Foo> fails as documented
+    b.foos[0].x = 100
+    assert b.foos[0].x == 100
