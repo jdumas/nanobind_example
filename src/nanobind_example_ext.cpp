7f81b1ec3c6b01c29e14b80f8275cbfca07522e1
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2025-08-18 14:59:33 -0700

    Minimal json example.

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,23 +1,17 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/bind_vector.h>
 #include <nanobind/stl/vector.h>
+#include <nanobind_json/nanobind_json.hpp>
 
 #include <string>
 
 namespace nb = nanobind;
+namespace nl = nlohmann;
 
 using namespace nb::literals;
 
 struct Foo {
-    int x;
-};
-
-struct Bar {
-    std::vector<Foo> foos;
-};
-
-struct Baz {
-    std::vector<int> ints;
+    nl::json data;
 };
 
 // Prefer binding over type caster for std::vector<Foo>
@@ -26,17 +20,7 @@ NB_MAKE_OPAQUE(std::vector<Foo>);
 NB_MODULE(nanobind_example_ext, m) {
     m.doc() = "This is a \"hello world\" example with nanobind";
 
-    nb::bind_vector<std::vector<Foo>>(m, "FooVector");
-
     nb::class_<Foo>(m, "Foo")
         .def(nb::init<>())
-        .def_rw("x", &Foo::x);
-
-    nb::class_<Bar>(m, "Bar")
-        .def(nb::init<>())
-        .def_rw("foos", &Bar::foos);
-
-    nb::class_<Baz>(m, "Baz")
-        .def(nb::init<>())
-        .def_rw("ints", &Baz::ints);
+        .def_rw("data", &Foo::data);
 }
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,28 +1,23 @@
 import nanobind_example as m
 
 
-def test_baz():
-    b = m.Baz()
-    assert len(b.ints) == 0
+def test_foo():
+    foo = m.Foo()
+    foo.data = dict()
 
-    # Using std::vector<int> type caster, appending to b.ints fails to mutate b.ints
-    b.ints.append(42)
-    assert len(b.ints) == 1
+    x = foo.data
+    x.update({"extension0": {"key": [0, 1, 2]}})
+    print(x)
+    assert len(x) == 1
 
-
-def test_bar():
-    b = m.Bar()
-    assert len(b.foos) == 0
-
-    f = m.Foo()
-    f.x = 42
-    assert f.x == 42
-
-    # Using std::vector<Foo> bindings, appendings to b.foos correctly mutates b.foos
-    b.foos.append(f)
-    assert len(b.foos) == 1
-    assert b.foos[0].x == 42
-
-    # However, mutating an element returned by a std::vector<Foo> fails as documented
-    b.foos[0].x = 100
-    assert b.foos[0].x == 100
+    foo.data.update({"extension0": {"key": [0, 1, 2]}})
+    print(foo.data)
+    assert len(foo.data) == 1
+    assert foo.data["extension0"] == {"key": [0, 1, 2]}
+    foo.data.update({"extension1": {"key": "foo"}})
+    assert foo.size == 2
+    assert foo.data["extension1"] == {"key": "foo"}
+    foo.data["extension0"]["key"][0] = 10
+    assert foo.data["extension0"] == {"key": [10, 1, 2]}
+    foo.data["extension0"]["key"] = [3, 4, 5]
+    assert foo.data["extension0"] == {"key": [3, 4, 5]}
