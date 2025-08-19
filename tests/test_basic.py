d6db0a4126c5117f5b8e918f1dd031a7774dd520
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2025-08-19 08:43:51 -0700

    Update unit tests.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,23 +1,32 @@
 import nanobind_example as m
 
 
-def test_foo():
+def test_operator():
     foo = m.Document()
     foo.data = dict()
 
-    x = foo.data
-    x.update({"extension0": {"key": [0, 1, 2]}})
-    print(x)
-    assert len(x) == 1
+    foo.data |= {"extension0": {"key": [0, 1, 2]}}
+    assert len(foo.data) == 1
+
+
+def test_update():
+    foo = m.Document()
+    foo.data = dict()
 
     foo.data.update({"extension0": {"key": [0, 1, 2]}})
-    print(foo.data)
     assert len(foo.data) == 1
     assert foo.data["extension0"] == {"key": [0, 1, 2]}
-    foo.data.update({"extension1": {"key": "foo"}})
-    assert foo.size == 2
+
+
+def test_modify():
+    foo = m.Document()
+    foo.data = {"extension0": {"key": [0, 1, 2]}, "extension1": {"key": "foo"}}
+    assert len(foo.data) == 1
+    assert foo.data["extension0"] == {"key": [0, 1, 2]}
     assert foo.data["extension1"] == {"key": "foo"}
+
     foo.data["extension0"]["key"][0] = 10
     assert foo.data["extension0"] == {"key": [10, 1, 2]}
+
     foo.data["extension0"]["key"] = [3, 4, 5]
     assert foo.data["extension0"] == {"key": [3, 4, 5]}
